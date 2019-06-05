2a06b2815140a35a507bab3ca67d503c6399da39
Dave Hawkins <dhawkins01@gmail.com>
2019-06-05 18:22:26 -0400

    question and answer array done

---
+++ assets/javascript/app.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+
+    // Question and answer array
+
+    var questions = [
+        {
+            question: "Which actor played Bruce Banner in the 2008 film 'The Incredible Hulk'?",
+            choices: ["Marc Ruffalo", "Edward Norton", "Robert Downey Jr.", "Dave Hawkins"],
+            correctChoice: "Edward Norton",
+            image: ""
+        },
+
+        {
+            question: "Which actor played Bruce Banner in the 2012 film 'The Avengers'?",
+            choices: ["Edward Norton", "Dave Hawkins", "Marc Ruffalo", "Robert Downey Jr."],
+            correctChoice: "Marc Ruffalo",
+            image: ""
+        },
+
+        {
+            question: "Who was the doctor that transformed Steve Rogers into Captain America?",
+            choices: ["Dr. Erskine", "Dr. Phil", "Dr. America", "Dr. Hawkins"],
+            correctChoice: "Dr. Erskine",
+            image: ""
+        },
+
+        {
+            question: "What is Captain America's shield made out of?",
+            choices: ["Titanium", "Hawkanium", "Aluminum", "Vibranium"],
+            correctChoice: "Vibranium",
+            image: ""
+        },
+
+        {
+            
+        }
+    ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
